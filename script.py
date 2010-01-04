3b322b9f98a17f0b987dcfa47cbc7d454a9dd7a8
korenyushkin <anton@akshell.com>
2010-01-04 20:11:06 +0300

    Happy new year!

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2009, Anton Korenyushkin
+# Copyright (c) 2009-2010, Anton Korenyushkin
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2009, Anton Korenyushkin
+# Copyright (c) 2009-2010, Anton Korenyushkin
 # All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
